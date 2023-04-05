9e16ecf4ccaa261fd4dec35967041b0201d1782a
Yuji Yamamoto <yujiyamamoto64@gmail.com>
2023-04-05 17:36:49 -0300

    Swagger

--- src/main/java/com/yujiyamamoto64/carrental/model/Car.java
+++ src/main/java/com/yujiyamamoto64/carrental/model/Car.java
@@ -3,11 +3,12 @@ package com.yujiyamamoto64.carrental.model;
 import java.io.Serializable;
 import java.util.Date;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Builder;
 import lombok.Data;
 
